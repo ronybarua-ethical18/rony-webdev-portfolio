5b7b8c139e5390c7d0ceb3ebe3b45c78de8bbeef
ronybarua.ethical18 <ronybarua.ethical18@gmail.com>
2021-05-03 11:17:27 +0600

    line height fixed

--- src/components/Home/HeaderContent/HeaderContent.js
+++ src/components/Home/HeaderContent/HeaderContent.js
@@ -9,14 +9,13 @@ const HeaderContent = () => {
                 <div className="col-md-10 offset-md-1 py-5 px-3">
                     <div className="w-100" id="nav-links">
                         <p className="title-color">Hi, my name is</p>
-                        <h1>Rony Barua.</h1>
-                        <h1><span>Jr. Web Developer</span></h1>
-                       {/* <div className="who-am-i mt-2">
+                        <Zoom><h1>Rony Barua.</h1></Zoom>
+                       <div className="who-am-i mt-2">
                             <Typed className="d-flex" strings={['<h1><span>Jr. Web Developer</span></h1>','<h1><span>I build Web apps</span></h1>']}
                             typeSpeed={50}
                             backSpeed={50}
                             loop />
-                       </div> */}
+                       </div>
                     <p className="bio-title">I have strong knowledge of front-end libraries and familiar with backend frameworks, using modern technologies I can develop dynamic websites for your company and I am 100% confident about my skills.</p>
                     <div className=""><a className="link" href="https://drive.google.com/uc?export=download&id=1qsOodYuTypzRCUSu3HqliUbpS_uZpkX5">Download Resume</a></div>
                 </div>
