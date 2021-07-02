09023e94636d7499fb6a7c318a10a34ba8ec60ec
ronybarua.ethical18 <ronybarua.ethical18@gmail.com>
2021-07-02 19:42:43 +0600

    navbar fixed

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -5,9 +5,9 @@ import HeaderMain from '../HeaderMain/HeaderMain'
 import HeaderContent from '../HeaderContent/HeaderContent';
 import About from '../About/About';
 import firebase from "firebase/app";
-import Fade from 'react-reveal/Fade';
 import "firebase/auth";
-import ScrollspyNav from "react-scrollspy-nav";
+import { Link } from 'react-scroll';
+import {animateScroll as scroll} from 'react-scroll'
 import Trainings from '../Traninings/Trainings';
 import FeaturedProjectsData from '../FeaturedProjectsData/FeaturedProjectsData';
 import MentionableProjects from '../MentionableProjects/MentionableProjects';
@@ -23,25 +23,18 @@ const Home = () => {
     return (
         <div className="main-home">
             <Navbar collapseOnSelect expand="lg" fixed="top" className="mx-4 py-3" id="navbar-bg">
-                <Navbar.Brand href="#home"><h2 className="title-color mr-auto animation-fade">R<span className="text-white">o</span>ny</h2></Navbar.Brand>
+                <Navbar.Brand href="#home" onClick={() => scroll.scrollToTop()}><h2 className="title-color mr-auto animation-fade">R<span className="text-white">o</span>ny</h2></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto" id="nav-links">
-                        <ScrollspyNav
-                            className="scrolling"
-                            scrollTargetIds={["about", "training", "featured", "other", "blogs", "contact"]}
-                            activeNavClass="is-active"
-                            headerBackground="true"
-                        >
                             <Nav className="ml-auto" id="nav-links">
-                                <a href="#about">About</a>
-                                <a href="#training">Trainings</a>
-                                <a href="#featured">Featured Projects</a>
-                                <a href="#other">Other Projects</a>
-                                <a href="#blogs">Blogs</a>
-                                <a href="#contact">Contact</a>
+                                <Link id="anchor" smooth={true} offset={-150} duration={1000} to="about">About</Link>
+                                <Link id="anchor" smooth={true} offset={-150} duration={1000} to="training">Trainings</Link>
+                                <Link id="anchor" smooth={true} offset={-150} duration={1000} to="featured">Featured Projects</Link>
+                                <Link id="anchor" smooth={true} offset={-150} duration={1000} to="other">Other Projects</Link>
+                                <Link id="anchor" smooth={true} offset={-150} duration={1000} to="blogs">Blogs</Link>
+                                <Link id="anchor" smooth={true} offset={-150} duration={1000} to="contact">Contact</Link>
                             </Nav>
-                        </ScrollspyNav>
                         <div><a href="https://bit.ly/3xFzoti" className="link">Resume</a></div>
                     </Nav>
                 </Navbar.Collapse>
